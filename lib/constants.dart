ae5f1e2a17b0d1a4780c957f8804476369511d0c
Ercan Tomac <48109156+ercantomac@users.noreply.github.com>
2022-12-25 23:04:03 +0100

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  //Cubic(0.18, 1.0, 0.15, 0.86) => FASTER ON START => MORE DRAMATIC DECELERATION
-  //Cubic(0.35, 0.91, 0.15, 0.86) => SLOWER ON START => MORE NATURAL DECELERATION
-  //Cubic(0.25, 0.46, 0.15, 0.86) => EVEN SLOWER ON START => VERY NATURAL DECELERATION
-  static Curve sizeCurve = const Cubic(0.25, 0.46, 0.15, 0.86),
-      sizeReverseCurve = Curves.decelerate,
-      alignmentCurve = const Cubic(0.18, 1.0, 0.15, 0.86),
-      alignmentReverseCurve = Curves.easeInOutSine;
-
-/*static Curve sizeCurve = const Cubic(0.25, 0.46, 0.15, 0.86),
-      sizeReverseCurve = Curves.linearToEaseOut,
-      alignmentCurve = Curves.easeOutQuint,
-      alignmentReverseCurve = const Cubic(0.25, 0.46, 0.15, 0.86);*/
-
-/*static Curve sizeCurve = const Cubic(0.25, 0.46, 0.15, 0.86),
-      sizeReverseCurve = const Cubic(0.25, 0.46, 0.15, 0.86),
-      alignmentCurve = Curves.easeOutQuint,
-      alignmentReverseCurve = const Cubic(0.25, 0.46, 0.15, 0.86);*/
+    static const Curve sizeCurve = Cubic(0.25, 0.1, 0.25, 1.0),
+      sizeReverseCurve = Curves.easeOutCubic,
+      alignmentCurve = Curves.easeOutCubic,
+      alignmentReverseCurve = Cubic(0.25, 0.1, 0.25, 1.0);
 }
