deb6c4113de32aa9d43acc8131a7ea0638c17385
Ercan Tomac <48109156+ercantomac@users.noreply.github.com>
2023-05-15 22:15:30 +0200

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constants {
-    static const Curve sizeCurve = Cubic(0.25, 0.1, 0.25, 1.0),
-      sizeReverseCurve = Curves.easeOutCubic,
-      alignmentCurve = Curves.easeOutCubic,
-      alignmentReverseCurve = Cubic(0.25, 0.1, 0.25, 1.0);
-}
